79e5637fc9d2e1905b8afcf26b80a8882e65c347
patriciahudakova <p.hudakova@reply.com>
2021-05-16 19:04:04 +0100

    refactoring and cli skeleton

--- pkg/types.go
+++ pkg/types.go
@@ -1,8 +1,19 @@
 package pkg
 
-// data mimics the fields we would find in the database
-type data struct {
-	userID string
-	domain string
-	hash   string
+import "time"
+
+// User mimics the fields we would find in the user database
+type User struct {
+	userID    int // primary key
+	firstName string
+	lastName  string
+}
+
+// Credentials mimics the fields we would find in a credentials database
+type Credentials struct {
+	domain   string // primary key
+	username string
+	password string
+	created  time.Time
+	userID   int // foreign key
 }
